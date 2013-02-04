fcce1f45af655a1d34429721b4b227ee89782d85
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-02-04 14:49:16 +1300

    Improved redirection code, updated resume, added several more projects.

--- pages/projects/controller.rb
+++ pages/projects/controller.rb
@@ -4,21 +4,13 @@ include Direct
 def on_redirect(path, request)
 	links = Links.index(BASE_PATH, Path.new)
 	
-	name = path.components[-2]
+	name = request['project']
 	
 	link = links.find{|link| link.name == name}
 	
 	if link and link.href
-		if link.href =~ /^\//
-			redirect! (URI_PATH + link.href).to_s
-		else
-			redirect! link.href
-		end
+		redirect! link.href
 	else
 		respond! 404
 	end
-end
-
-def process!(path, request)
-	passthrough(path, request)
 end

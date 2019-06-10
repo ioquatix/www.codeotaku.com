ea2314d02694a159b6f365fa6b496d294a8c0fc9
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-06-11 00:44:06 +1200

    Update falcon configuration.

--- falcon.rb
+++ falcon.rb
@@ -1,8 +1,7 @@
+#!/usr/bin/env -S falcon host
 
-rack 'www.codeotaku.com', :self_signed do
-	root {File.realpath(__dir__)}
-	config_path {::File.expand_path("config.ru", root)}
-	
-	# ssl_certificate_path '/etc/letsencrypt/live/www.codeotaku.com/fullchain.pem'
-	# ssl_private_key_path '/etc/letsencrypt/live/www.codeotaku.com/privkey.pem'
-end
+load :rack, :lets_encrypt, :supervisor
+
+rack 'www.codeotaku.com', :lets_encrypt
+
+supervisor

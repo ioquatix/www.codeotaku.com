3491fff06a601a1e37712ed7bcace0b0c2622de0
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-04-14 14:07:59 +1200

    Add results & conclusion.

--- pages/journal/2020-04/ruby-concurrency-final-report/http.rb
+++ pages/journal/2020-04/ruby-concurrency-final-report/http.rb
@@ -1,8 +1,21 @@
-#!/usr/bin/env RUBY_VERSION=ruby-thread-scheduler
-
-TOPICS = ["nonblocking", "ruby", "is", "so", "great"]
-def fetch(urls)
-	urls.each do |url|
-		response = Net::HTTP.get(url)
+def fetch_topics(topics)
+	counts = {}
+	
+	conditions = topics.map do |topic|
+		condition = Scheduler::Condition.new
+		
+		Fiber.new(blocking: Fiber.current.blocking?) do
+			uri = URI("https://www.google.com/search?q=#{topic}")
+			counts[topic] = Net::HTTP.get(uri).scan(topic).size
+			
+			condition.signal
+		end.resume
+		
+		condition
 	end
+	
+	# Wait for all requests to finish:
+	conditions.each(&:wait)
+	
+	return counts
 end

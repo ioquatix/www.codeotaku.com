bead18ace0e13988921653de26c0b2c5b8d41f1f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-04-14 21:26:59 +1200

    Rework structure of report.

--- pages/journal/2020-04/ruby-concurrency-final-report/fiber-blocking.rb
+++ pages/journal/2020-04/ruby-concurrency-final-report/fiber-blocking.rb
@@ -1,8 +1,12 @@
-# The default:
-Fiber.new(blocking: true) do
-	puts Fiber.current.blocking? # true
-end.resume
-
 Fiber.new(blocking: false) do
 	puts Fiber.current.blocking? # false
+	
+	# May invoke `Thread.scheduler&.wait_readable`.
+	io.read(...)
+	
+	# May invoke `Thread.scheduler&.wait_writable`.
+	io.write(...)
+	
+	# Will invoke `Thread.scheduler&.wait_sleep`.
+	sleep(n)
 end.resume
--- pages/journal/2020-04/ruby-concurrency-final-report/thread-scheduler.rb
+++
@@ -1,8 +0,0 @@
-scheduler = Scheduler.new # implementation defined
-Thread.current.scheduler = scheduler
-
-Fiber do
-	# Equivalent to:
-	# 	Thread.scheduler&.wait_readable(io.filedes)
-	io.wait_readable
-end

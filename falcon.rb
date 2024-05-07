15240a8b061a02dde74a7d924b0f75ec241c594d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-05-08 01:19:45 +1200

    Add missing TLS environment.

--- falcon.rb
+++ falcon.rb
@@ -5,11 +5,13 @@
 # Copyright, 2019-2022, by Samuel Williams.
 
 require 'falcon/environment/rack'
+require 'falcon/environment/tls'
 require 'falcon/environment/lets_encrypt_tls'
 
 hostname = File.basename(__dir__)
 
 service hostname do
 	include Falcon::Environment::Rack
+	include Falcon::Environment::TLS
 	include Falcon::Environment::LetsEncryptTLS
 end

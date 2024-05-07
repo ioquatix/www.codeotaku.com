1e350ba146b115b9e8056a797931acb6e4f28faa
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-05-08 00:37:13 +1200

    Require missing environments.

--- falcon.rb
+++ falcon.rb
@@ -4,6 +4,9 @@
 # Released under the MIT License.
 # Copyright, 2019-2022, by Samuel Williams.
 
+require 'falcon/environment/rack'
+require 'falcon/environment/lets_encrypt_tls'
+
 hostname = File.basename(__dir__)
 
 service hostname do

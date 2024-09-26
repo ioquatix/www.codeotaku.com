0436152b6b3f5784dbf47f1bd561b599c01fc456
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-09-27 00:43:23 +1200

    Improve robustness of git version extraction.

--- config/datadog.rb
+++ config/datadog.rb
@@ -3,11 +3,27 @@
 # Released under the MIT License.
 # Copyright, 2024, by Samuel Williams.
 
-require 'datadog'
+require "datadog"
+
+require "open3"
+require "console/event/failure"
+
+def git_version(path = "/usr/bin/git")
+	stdout, status = Open3.capture2(path, "describe", "--always")
+	
+	if status.success?
+		yield stdout.strip
+	end
+rescue => error
+	Console::Event::Failure.for(error).emit(self)
+end
 
 Datadog.configure do |config|
-	config.service = 'utopia'
-	config.version = `git describe --always`.strip
-	
 	config.logger.level = Logger::WARN
+	
+	config.service = 'utopia'
+	
+	git_version do |version|
+		config.version = version
+	end
 end
